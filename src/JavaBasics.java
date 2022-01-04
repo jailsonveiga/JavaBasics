a0d5fe607ef4ce76e804c6aedba4f48b544c9be0
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-03 20:33:50 -0500

    JavaBasics.java / string concatenation

---
+++ src/JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics {
+    public static void main(String[] args){
+        String firstName = "Jay";
+        String lastName = "Veiga";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+
+}
