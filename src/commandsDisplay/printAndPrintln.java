9e714e7158a29da8df07f876466d76e1acd75b1a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-27 20:53:25 -0400

    Difference Of Print And Println Command

---
+++ src/commandsDisplay/printAndPrintln.java
@@ -0,0 +1,21 @@
+package commandsDisplay;
+
+public class printAndPrintln {
+    public static void main(String[] args) {
+        //Display "Hello World" On The Screen
+        System.out.println ("Hello World");
+
+        //Display The Number "1" On The Screen
+        System.out.println (1);
+
+        //Difference of println and print command
+
+        System.out.print ("Hello Guys ");
+         //Line 13 and 16 combined
+        //Display "Hello Guys" "I'm 18 Years Old" On The Screen
+        System.out.println("I'm 18 Years Old");
+
+        //Display "Bye" On The Screen
+        System.out.print ("Bye!!");
+    }
+}
