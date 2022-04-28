ffe43ec0d0f8a3be4f4fd3c653180fb4107d9b0a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-27 20:52:52 -0400

    Print Command In One Line

---
+++ src/commandsDisplay/printCommand.java
@@ -0,0 +1,8 @@
+package commandsDisplay;
+
+public class printCommand {
+    public static void main (String[] args) {
+        //Display "I think being a programmer is cool" On The Screen in one line
+        System.out.print ("I think being a programmer is cool");
+    }
+}
