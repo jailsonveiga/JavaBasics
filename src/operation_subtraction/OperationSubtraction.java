f0798f911d6a1e01c610fc60b8c749c890146c5b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-28 23:46:50 -0400

    Work With Subtraction

---
+++ src/operation_subtraction/OperationSubtraction.java
@@ -0,0 +1,13 @@
+package operation_subtraction;
+
+public class OperationSubtraction {
+    public static void main (String[] args) {
+        int yearCurrently = 3126;
+
+        int yearBorn = 8;
+
+        int text = yearCurrently - yearBorn;
+
+        System.out.println(text);
+    }
+}
