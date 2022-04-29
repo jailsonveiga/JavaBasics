99a939bdea10388ffef6ae40038e60c9e3bf0e37
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-28 23:46:25 -0400

    Work With Command PrintLn

---
+++ src/command_println/CommandPrintLn.java
@@ -0,0 +1,45 @@
+package command_println;
+
+public class CommandPrintLn {
+    public static void main(String[] args) {
+        //Print "Jay is very smart" 5 times
+        //Display "1) Jay is very smart" On The Screen"
+        System.out.println("1) Jay is very smart");
+
+        //Display "2) Jay is very smart" On The Screen
+        System.out.println("2) Jay is very smart");
+
+        //Display "3) Jay is very smart" On The Screen
+        System.out.println("3) Jay is very smart");
+
+        //Display "4) Jay is very smart" On The Screen
+        System.out.println("4) Jay is very smart");
+
+        //Display "5) Jay is very smart" On The Screen
+        System.out.println("5) Jay is very smart");
+
+        //Display "-----" On The Screen
+        System.out.println("-----");
+
+        //Initialize String DataType with test Variable name and assign the value "Jay is very smart" and store into a test variable
+        String test = "Jay is very smart";
+
+        //Print "Jay is very smart" 5 times
+        //Display "1) " and join "+" the variable test value on the screen
+        System.out.println("1) " + test);
+
+        //Display "2) " and join "+" the variable test value on the screen
+        System.out.println("2) " + test);
+
+        //Display "3) " and join "+" the variable test value on the screen
+        System.out.println("3) " + test);
+
+        //Display "4) " and join "+" the variable test value on the screen
+        System.out.println("4) " + test);
+
+        //Display "5) " and join "+" the variable test value on the screen
+        System.out.println("5) " + test);
+
+
+    }
+}
