4318a4cac2ea63dd0e186e85238f5eb9583f13f1
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-30 21:28:07 -0400

    Display "2 plus 3 is equal to 5" on the screen

---
+++ src/two_plus_three_is_equal_to_five/TwoPlusThreeEqualToFive.java
@@ -0,0 +1,27 @@
+package two_plus_three_is_equal_to_five;
+
+public class TwoPlusThreeEqualToFive {
+    public static void main (String[] args) {
+
+        //Initialize integer datatype with variable name a and assign value 2 and store into variable a
+        int a = 2;
+
+        //Initialize integer datatype with variable name b and assign value 3 and store into variable b
+        int b = 3;
+
+        //Display 2 on the screen
+        System.out.print (a);
+
+        //Display 2 " plus " on the screen
+        System.out.print (" plus ");
+
+        //Display 2 " plus " 3 on the screen
+        System.out.print (b);
+
+        //Display 2 " plus " 3 " is equal to " on the screen
+        System.out.print (" is equal to ");
+
+        //Display 2 " plus " 3 " is equal to " 5 on the screen
+        System.out.print (a + b);
+    }
+}
