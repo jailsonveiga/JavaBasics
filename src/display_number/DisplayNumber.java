36f65b7ae9421f0a77d41c4b0bdb122009426d90
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-30 21:10:24 -0400

    Display Numbers that output value 12 for x and value 2 for y on the screen

---
+++ src/display_number/DisplayNumber.java
@@ -0,0 +1,26 @@
+package display_number;
+
+public class DisplayNumber {
+    public static void main(String[] args) {
+        //Initialize the integer datatype with variable name x and assign the value 2 and store into the variable x
+        int x = 2;
+
+        //Initialize the integer datatype with variable name y and assign the value 12 and store into the variable y
+        int y = 12;
+
+        //Add this 2 variable values x "2" and y "12" and store the value into variable y
+        y = x + y;
+
+        //Subtract this 2 variable values y "14" and x "2" and store the value into variable x
+        x = y - x;
+
+        //Add this 2 variable values y "2" and x "12" and store the value into variable y
+        y = y - x;
+
+        //Display the value 12 on the screen
+        System.out.println(x);
+
+        //Display the value 2 on the screen
+        System.out.println(y);
+    }
+}
