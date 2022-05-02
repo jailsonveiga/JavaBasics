b9ea60bbe6bb38e61be586673809df0227df7d1e
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-01 23:24:47 -0400

    Display String Text on the screen

---
+++ src/command_print/CommandPrintString.java
@@ -0,0 +1,8 @@
+package command_print;
+
+public class CommandPrintString {
+    public static void main (String[] args) {
+        //Display String Text on the screen
+        System.out.print("It's not a bug - it's a feature.");
+    }
+}
