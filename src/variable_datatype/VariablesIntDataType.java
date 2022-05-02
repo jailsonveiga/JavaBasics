ac30f409315dba62a787e4e3fe085d60fc731bcc
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-02 11:13:30 -0400

    Display Variable datatype

---
+++ src/variable_datatype/VariablesIntDataType.java
@@ -0,0 +1,17 @@
+package variable_datatype;
+
+public class VariablesIntDataType {
+    public static void main (String[] args) {
+        //Initialize Integer datatype and assign the value of number 12 and store the value into variable name a
+        int a = 12;
+
+        //Initialize Integer datatype and assign the value of number 5 and store the value into variable name b
+        int b = 5;
+
+        //Display the value of variable a "12" on the screen
+        System.out.println (a);
+
+        //Display the value of variable b "5" on the screen
+        System.out.println (b);
+    }
+}
