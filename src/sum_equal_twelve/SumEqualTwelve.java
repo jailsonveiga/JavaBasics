5f504db44747d593560216405ba8c3de7de8f5b6
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-01 22:54:25 -0400

    Display Sum Equal 12 on the screen

---
+++ src/sum_equal_twelve/SumEqualTwelve.java
@@ -0,0 +1,17 @@
+package sum_equal_twelve;
+
+public class SumEqualTwelve {
+    public static void main (String[] args) {
+        //Initialize integer datatype with variable name a and assign value of 3 and store into variable a
+        int a = 3;
+
+        //Initialize integer datatype with variable name b and assign value of 5 and store into variable b
+        int b = 5;
+
+        // add sum of 4 + value of a "3" plus value of "5" and the total sum store in a variable sum
+        int sum = 4 + a + b;
+
+        //Display "Sum = 12" on the screen
+        System.out.print("Sum = " + sum);
+    }
+}
