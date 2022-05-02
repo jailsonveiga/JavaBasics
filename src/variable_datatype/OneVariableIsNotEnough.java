85ebebbf8f67f825fbdaf1117ae90074315b53a2
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-02 11:12:51 -0400

    Display more than One Variable DataType

---
+++ src/variable_datatype/OneVariableIsNotEnough.java
@@ -0,0 +1,17 @@
+package variable_datatype;
+
+public class OneVariableIsNotEnough {
+    public static void main (String[] args) {
+        //Initialize the integer datatype and give the variable name a and assign 26 and store the value into variable a
+        int a = 26;
+
+        //Initialize the integer datatype and give the variable name b and assign 13 and store the value into variable a
+        int b = 13;
+
+        //Initialize the String datatype and give the variable name s and assign "Hello Java" and store the value into variable s
+        String s = "Hello Java Version";
+
+        //Display "Hello Java Version 26 and 13" on the screen
+        System.out.print(s + " " + a + " and " + b);
+    }
+}
