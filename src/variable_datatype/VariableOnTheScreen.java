f1dba2cfd47284504340a2354d8fc046f88618ad
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-02 11:13:18 -0400

    Display Variable

---
+++ src/variable_datatype/VariableOnTheScreen.java
@@ -0,0 +1,11 @@
+package variable_datatype;
+
+public class VariableOnTheScreen {
+    public static void main (String[] args) {
+        //Initialize the String datatype and call the variable name and assign "Hello Java Programmers" and store the value into our variable name
+        String name = "Hello Java Programmers";
+
+        //Display the value of the variable name on the screen
+        System.out.print (name);
+    }
+}
