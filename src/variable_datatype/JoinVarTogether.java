0f8154d2b36e085bdd6b3984ddc2323bfef630af
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-04 11:57:44 -0400

    Joying Variables together

---
+++ src/variable_datatype/JoinVarTogether.java
@@ -0,0 +1,15 @@
+package variable_datatype;
+
+public class JoinVarTogether {
+    public static void main (String[] args) {
+        //Display text on the screen using variables names
+
+        //Initializing 3 variables name iLike, toCode, inJava using String datatype and assign the values and store into a specific variable
+        String iLike = "I like";
+        String toCode = "to code";
+        String inJava = "in Java";
+
+        //Displaying variable values on the screen also using a + operator to joying the text together and add a space between the words
+        System.out.print (iLike + " " + toCode + " " + inJava);
+    }
+}
