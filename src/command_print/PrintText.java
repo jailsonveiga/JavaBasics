23d7abdd298c159268d328eb76d662e1582fc0ab
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-01 15:52:38 -0400

    Display a string literal text on the screen

---
+++ src/command_print/PrintText.java
@@ -0,0 +1,8 @@
+package command_print;
+
+public class PrintText {
+    public static void main (String[] args) {
+        //Display String Literal value "Hello Java World" on the screen
+        System.out.println("Hello Java World");
+    }
+}
