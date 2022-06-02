db7ef2cb3563fb85c3b2d456c1b0dd2faf89ea74
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-02 15:58:52 -0400

    Display the following text on the screen

---
+++ src/command_print/DisplayTextOnTheScreen.java
@@ -0,0 +1,11 @@
+package command_print;
+
+public class DisplayTextOnTheScreen {
+    public static void main (String[] args) {
+        //Display the following text on the screen
+        System.out.println ("My name is Jay");
+        System.out.println ("I agree to learn Java");
+        System.out.println("I will be studying everyday");
+        System.out.println("to learn the java programming");
+    }
+}
