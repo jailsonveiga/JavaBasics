5df1ab6dd3fb8f9efc891da4b6db18f7d33a7cf3
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-14 23:00:38 -0400

    Integer DataType and Perform Operations On Them Addiction, Subtraction, Multiplication, Division

---
+++ src/variable_datatype/IntOperations.java
@@ -0,0 +1,51 @@
+package variable_datatype;
+
+public class IntOperations {
+
+    public static void main (String[] args) {
+        //Integer Type lets you store numbers in variables and perform operations on them addition, subtraction, multiplication, division, etc...
+
+        //Initialize an Integer DataType and Assign Value number 5 and Store into Variable a
+        int a = 5;
+
+        //Initialize an Integer DataType and Assign Value of a and add 5 to it and store into variable b
+        int b = a + 5;
+
+        //Display The value of b On The Screen
+        System.out.println(b);
+
+        //Display "-----" On The Screen
+        System.out.println("-----");
+
+        //Initialize Integer DataType with variable name c and Assign the value of b - 5 and Store into variable c
+        int c = b - 5;
+
+        //Display The Value Of c On The Screen
+        System.out.println(c);
+
+        //Display "-----" On The Screen
+        System.out.println("-----");
+
+        //Initialize Integer DataType with variable name d and assign value 10 and store into variable d
+        int d = 10;
+
+        //Initialize Integer DataType with variable name d and assign value 8 and store into variable e
+        int e = 8;
+
+        //Initialize Integer f and assign the value of d - e and e + d and multiply both value and store into variable f
+        int f = (d - e) * (e + d);
+
+        //Display The Value Of c On The Screen
+        System.out.println(f);
+
+        //Display "-----" On The Screen
+        System.out.println("-----");
+
+        //Initialize Integer DataType with variable name g and Assign the value of f / 2 and store into variable g
+        int g = f / 2;
+
+        //Display the value of g On The Screen
+        System.out.println (g);
+
+    }
+}
