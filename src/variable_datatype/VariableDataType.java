264626ec725f4fe69c300beef047d3e6575f36f1
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-14 23:01:29 -0400

    Work With int and String DataType

---
+++ src/variable_datatype/VariableDataType.java
@@ -0,0 +1,25 @@
+package variable_datatype;
+
+public class VariableDataType {
+    public static void main (String[] args) {
+        //Declare a Variable and Its DataType
+
+        //Declaring an Integer DataType With Variable Name a
+        int a;
+
+        //Declaring an String DataType With Variable Name s
+        String s;
+
+        //Assign Value 15 into Variable a
+        a = 15;
+
+        //Assign Value "Hello World" into Variable s
+        s = "Hello World";
+
+        //Display 15 On The Screen
+        System.out.println (a);
+
+        //Display "Hello World" On The Screen
+        System.out.println (s);
+    }
+}
