943df24535fac30d3627b37d556cc7816611b5a7
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-21 23:36:02 -0400

    Use the List array and collections class and sort method to sort the list of numbers using input from the user

---
+++ src/array_list/IntegerArrayListUserInput.java
@@ -0,0 +1,33 @@
+package array_list;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class IntegerArrayListUserInput {
+    public static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a number?");
+        int num1 = Integer.parseInt(userInput.readLine());
+
+        System.out.println("Enter a number?");
+        int num2 = Integer.parseInt(userInput.readLine());
+
+        System.out.println("Enter a number?");
+        int num3 = Integer.parseInt(userInput.readLine());
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(num1);
+        numbers.add(num2);
+        numbers.add(num3);
+
+        Collections.sort(numbers);
+
+        System.out.println("The number you enter is: " + num1 + "-" + num2 + "-" + num3 + " and the sort number is: " + numbers);
+    }
+}
