40bfa747b63229910881c5d7568307e32996e21b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-22 09:10:47 -0400

    Use the List array and sort method to sort the list of names using input from the user in descending order

--- src/array_list/StringIntArrayListUserInput.java
+++ src/array_list/StringIntArrayListUserInput.java
@@ -8,26 +8,38 @@ import java.util.Collections;
 import java.util.List;
 
 public class StringIntArrayListUserInput {
+    //Add BufferedReader java class to read text from the keyboard
     public static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
 
+        //Display message on the screen
         System.out.println("Enter your name?");
+        //Initialize String datatype name1 and assign the value from the user input and store the value into name1 variable
         String name1 = userInput.readLine();
 
+        //Display message on the screen
         System.out.println("Enter your name?");
+        //Initialize String datatype name2 and assign the value from the user input and store the value into name2 variable
         String name2 = userInput.readLine();
 
+        //Display message on the screen
         System.out.println("Enter your name?");
+        //Initialize the String datatype name3 and assign the value from the user input and store the value into name3 variable
         String name3 = userInput.readLine();
 
+        //Create an empty array list and assign the future value and store the value into names
         List<String> names = new ArrayList<>();
 
+        //Add name1, name2, name3 value and store the value into names
         names.add(name1);
         names.add(name2);
         names.add(name3);
 
+        //Use Sort method and java class Collections to sort names in reverse order "descending order"
         names.sort(Collections.reverseOrder());
 
+        //Display the message on the screen and show the value from the variable, also use the + operator to join the text together
         System.out.println("Sort names in descending order :\n" + names);
 
     }
