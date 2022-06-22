1fd5614e0494dbc1e2636d9ba5657ade9d231725
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-22 16:42:29 -0400

    Working with loop

---
+++ src/loops/WhileLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        //Initialize Integer data type timer and assign value 10 and store the value into timer
+        int timer = 10;
+
+        //Check if timer value 10 is greater than 0,
+        while(timer > 0) {
+            //Display message on the screen
+            System.out.println(timer);
+            //Decrease timer after each iterator
+            timer--;
+        }
+    }
+}
