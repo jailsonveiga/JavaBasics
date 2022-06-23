23685c238512fda53a049d204154b24d2d0f0aff
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-22 20:27:01 -0400

    Working with user input "Prediction"

--- src/scanner/ScannerPredictions.java
+++ src/scanner/ScannerPredictions.java
@@ -6,19 +6,22 @@ import java.io.InputStreamReader;
 
 public class ScannerPredictions {
     public static void main (String[] args) throws IOException {
-        //Create a new intense object and read data from the keyboard
+        //Create a new intense object java clas to read data from the keyboard
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.println("Please enter your name?");
         //Initialize a String DataType variable name and store the value from the keyboard into variable name
         String name = userInput.readLine();
 
+        System.out.println("Please enter amount?");
         //Initialize a String DataType variable money and store the value from the keyboard into variable money
         String money = userInput.readLine();
 
+        System.out.println("Please enter year?");
         //Initialize a String DataType variable years and store the value from the keyboard into variable years
         String years = userInput.readLine();
 
         //Display the value store from the variable name, money and years and use the + operator to join the text
-        System.out.println(name + " will receive " + money + " in " + years + " years.");
+        System.out.println(name + " will receive " + money + " dollars in " + years + " years.");
     }
 }
