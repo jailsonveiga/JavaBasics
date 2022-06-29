938db7d73324907fd34554d6290e33ff1085786d
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-29 19:01:28 -0400

    This loop will print numbers from 1 to 10 inclusive:inclusive

---
+++ src/for_loop/ForLoopOneToTen.java
@@ -0,0 +1,13 @@
+package for_loop;
+
+public class ForLoopOneToTen {
+    public static void main(String[] args) {
+        //Initialize data type i and assign initial value to 1 and store the value into i
+        //Check if i value is less than equal 10
+        //add i value by 1 after each iteration
+        for (int i = 1; i <= 10 ; i++) {
+            //Display message on the screen
+            System.out.println("Print number " + i);
+        }
+    }
+}
