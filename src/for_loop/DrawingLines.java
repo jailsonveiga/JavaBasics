12d6eb4912ba089dc3006e0a7fc151d0b326002e
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-28 20:37:48 -0400

    Display message on the screen horizontal and vertical

---
+++ src/for_loop/DrawingLines.java
@@ -0,0 +1,20 @@
+package for_loop;
+
+public class DrawingLines {
+    public static void main(String[] args) {
+        //Initialize data type i and assign initial value to 1 and store the value into i
+        //Check if i value is less than equal 10
+        //add i value by 1 after each iteration
+        for (int i = 0; i < 10; i++) {
+            //Display message horizontal on the screen
+            System.out.print("8");
+        }
+        //Initialize data type i and assign initial value to 1 and store the value into i
+        //Check if i value is less than equal 10
+        //add i value by 1 after each iteration
+        for (int i = 0; i < 10; i++) {
+            //Display message vertical on the screen
+            System.out.println("8");
+        }
+    }
+}
