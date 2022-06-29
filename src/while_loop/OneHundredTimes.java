74958105d182a84694d466314bb7254be9467e33
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-29 19:38:40 -0400

    This loop will print numbers from 1 to 100 inclusive

---
+++ src/while_loop/OneHundredTimes.java
@@ -0,0 +1,16 @@
+package while_loop;
+
+public class OneHundredTimes {
+    public static void main(String[] args) {
+        //Initialize data type i and assign value 1 and store the value into variable i
+        int i = 1;
+
+        //As long value i is less or equal 100 continue the iteration
+        while(i <= 100) {
+            //Display message on the screen
+            System.out.println("Print number: " + i);
+            //increase i by 1 after each iteration
+            i++;
+        }
+    }
+}
