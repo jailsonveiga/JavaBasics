67cdf2f04281c2b10bf5dae8fc0fd73176419239
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-29 19:32:29 -0400

    This loop will print numbers from 10 to 5 inclusive

--- src/for_loop/ForLoopTenToOne.java
+++ src/for_loop/ForLoopTenToOne.java
@@ -2,7 +2,11 @@ package for_loop;
 
 public class ForLoopTenToOne {
     public static void main(String[] args) {
+        //Initialize data type i and assign initial value to 10 and store the value into i
+        //Check if i value is less than equal 5
+        //subtract i value by 1 after each iteration
         for (int i = 10; i >= 5 ; i--) {
+            //Display message on the screen
             System.out.println("Print number " + i);
         }
     }
