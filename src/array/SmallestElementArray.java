8c2995800093c286f3b8d2527b2a03c3f5a44309
jailsonveiga <jmdaveiga2012@gmail.com>
2022-06-29 23:53:23 -0400

    Find the smallest element in an array

--- src/array/SmallestElementArray.java
+++ src/array/SmallestElementArray.java
@@ -2,8 +2,10 @@ package array;
 
 public class SmallestElementArray {
     public static void main(String[] args) {
+        //Array of int that holds 11 elements
         int[] list = {5, 6, 7, 8, 1, 2, 5, -7, -9, 2, 0};
 
+        //Initialize integer data type min and assign the value of list on index 0 and store the value into min variable
         int min = list[0];
         for (int i = 1; i < list.length; i++) {
             if (list[i] < min)
