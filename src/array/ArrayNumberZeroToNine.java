ccddeeeb7984a356f9839750f5c04e049ca926dd
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-01 12:27:28 -0400

    Fill a 10 element array with number from 0 to 9

---
+++ src/array/ArrayNumberZeroToNine.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ArrayNumberZeroToNine {
+    public static void main(String[] args) {
+        int[] arrNum = new int[10];
+
+        for (int i = 0; i < arrNum.length; i++) {
+            System.out.println("Number: " + (arrNum[i] = i ));
+
+        }
+    }
+}
