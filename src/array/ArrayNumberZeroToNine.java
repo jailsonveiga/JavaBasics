7282d8db68eabc5b9d838e4b520efa479dd814ee
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-01 12:30:14 -0400

    Fill a 10 element array with number from 0 to 9

--- src/array/ArrayNumberZeroToNine.java
+++ src/array/ArrayNumberZeroToNine.java
@@ -2,9 +2,14 @@ package array;
 
 public class ArrayNumberZeroToNine {
     public static void main(String[] args) {
+        //Integer data type array that holds 10 elements
         int[] arrNum = new int[10];
 
+        //use a for loop, starting i with initial value of 0
+        //Check if i value is less than arrNum length
+        //Increment i value by 1 after each iteration
         for (int i = 0; i < arrNum.length; i++) {
+            //After each iteration store the value of i into arrNum array
             System.out.println("Number: " + (arrNum[i] = i ));
 
         }
