b19d2834160fed8c6f3e71c018b694553f4989d5
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-01 12:02:30 -0400

    Fill a 10 element array with number from 1 to 10

--- src/array/ArrayNumberOneToTen.java
+++ src/array/ArrayNumberOneToTen.java
@@ -2,6 +2,15 @@ package array;
 
 public class ArrayNumberOneToTen {
     public static void main(String[] args) {
+        //Integer data type array that holds 10 elements
+      int[] arrNum = new int[10];
 
+         //use a for loop, starting i with initial value of 0
+        //Check if i value is less than arrNum length
+        //Increment 1 after each iteration
+      for(int i = 0; i < arrNum.length; i++) {
+          //Display the message on the screen
+          System.out.println("Number: " + (arrNum[i] = i + 1));
+      }
     }
 }
