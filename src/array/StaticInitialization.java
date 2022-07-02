604cc026b420c985d1beb525e80d28e1eefba3ea
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-02 13:58:12 -0400

    Static array initialization

---
+++ src/array/StaticInitialization.java
@@ -0,0 +1,12 @@
+package array;
+
+public class StaticInitialization {
+    public static void main(String[] args) {
+
+        int[] arrNum = {5, 6, 7, 8, 1, 2, 5, -7, -9, 2, 0};
+
+        for (int i = 0; i < arrNum.length; i++) {
+            System.out.println(arrNum[i]);
+        }
+    }
+}
