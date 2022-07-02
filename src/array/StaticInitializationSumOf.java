9cc79374035e0a33a7d27eac89258e9122f60097
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-02 14:06:23 -0400

    Static array initialization add up array element

--- src/array/StaticInitializationSumOf.java
+++ src/array/StaticInitializationSumOf.java
@@ -10,6 +10,9 @@ public class StaticInitializationSumOf {
         //Initialize a integer data type sum and assign the initial value to 0 and store the value into sum variable
         int sum = 0;
 
+        //use a for loop, starting i with initial value of 0
+        //Check if i value is less than arrNum length
+        //Increment i value by 1 after each iteration
         for (int i = 0; i < arrNum.length; i++) {
             //Calculate the sum
             sum += arrNum[i];
