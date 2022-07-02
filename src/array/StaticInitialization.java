916156649c3c7678ef7837287d3f20287e1504cb
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-02 13:59:04 -0400

    Static array initialization

--- src/array/StaticInitialization.java
+++ src/array/StaticInitialization.java
@@ -3,8 +3,11 @@ package array;
 public class StaticInitialization {
     public static void main(String[] args) {
 
+        //Integer array data type that hold this elements
+        //Static initialization
         int[] arrNum = {5, 6, 7, 8, 1, 2, 5, -7, -9, 2, 0};
 
+        //Display arrNum array elements on the screen
         for (int i = 0; i < arrNum.length; i++) {
             System.out.println(arrNum[i]);
         }
