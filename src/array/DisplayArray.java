904d6907c08f20e18abcafdc966b912f1770812d
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-02 13:52:40 -0400

    Display an array on the screen

--- src/array/DisplayArray.java
+++ src/array/DisplayArray.java
@@ -2,11 +2,14 @@ package array;
 
 public class DisplayArray {
     public static void main(String[] args) {
+        //Integer array that hold 10 elements
         int[] arrNum = new int[10];
 
+        //Fill the array
         for (int i = 0; i < arrNum.length; i++) {
             arrNum[i] = i;
         }
+        //Display the contents of the array
         for (int i = 0; i < arrNum.length; i++) {
             System.out.println(arrNum[i]);
         }
