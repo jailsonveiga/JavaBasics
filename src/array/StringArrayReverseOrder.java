cc5a782bee004c4b4d811fcfc984bb0139a1ad58
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-03 14:49:27 -0400

    String array in reverse order "read text from the keyboard"

--- src/array/StringArrayReverseOrder.java
+++ src/array/StringArrayReverseOrder.java
@@ -1,6 +1,11 @@
 package array;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class StringArrayReverseOrder {
+    //Add BufferedReader java class to read text from the keyboard
+    public static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
 
     }
