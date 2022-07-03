c6e79dfdce224348eb9d136bbbf35000e54887e7
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-03 14:38:33 -0400

    Find the smallest element in an array "Sorted first"

--- src/array/FindSmallestElement.java
+++ src/array/FindSmallestElement.java
@@ -4,13 +4,16 @@ import java.util.Arrays;
 
 public class FindSmallestElement {
     public static void main(String[] args) {
-
+        //Static initialization
         int[] arrNum = {5, 6, 7, 8, 1, 2, 5, -7, -9, 2, 0};
 
+        //assign the arrNum length array and store the value into size variable
         int size = arrNum.length;
 
+        //Sort arrNum array
         Arrays.sort(arrNum);
 
+        //Display message on the screen
         System.out.println("Sorted Array: " + Arrays.toString(arrNum));
 
     }
