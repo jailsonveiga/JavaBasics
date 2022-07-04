b059437ef794d1df3881dff9d37689038cd2ebb2
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-04 19:47:46 -0400

    String array in reverse order

--- src/array/StringArrayReverseOrder.java
+++ src/array/StringArrayReverseOrder.java
@@ -15,12 +15,20 @@ public class StringArrayReverseOrder {
         //Display the message on the screen
         System.out.println("Enter a text?");
 
+        //use a for loop, starting i with initial value of 0
+        //Check if i value is less than 10
+        //Increment i value by 1 after each iteration
         for (int i = 0; i < 10; i++) {
+            //Read text from the user input and store the value into arrName array
             arrName[i] = userInput.readLine();
         }
 
+        //use a for loop, starting i with initial value of 9
+        //Check if i value is greater than or equal 0
+        //decrement  i value by 1 after each iteration
         for (int i = 9; i >= 0; i--) {
-            System.out.println(arrName[i]);
+            //Display message on the screen
+            System.out.println("Reverse order array elements: " + i + " " + arrName[i]);
         }
     }
 }
