1ae9cea9a9823c2c6933f2be5a971ac2c49ca965
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-04 19:51:49 -0400

    Array that hold 10 elements

--- src/array/NumberArrayReverseOrder.java
+++ src/array/NumberArrayReverseOrder.java
@@ -3,5 +3,7 @@ package array;
 public class NumberArrayReverseOrder {
     public static void main(String[] args) {
 
+        //integer data type array that hold 10 elements
+        int[] arrNum = new int [10];
     }
 }
