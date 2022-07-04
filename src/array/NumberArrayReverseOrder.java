0558c9afb7518ed5f673ba7e82e9598c50ca1f62
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-04 19:53:55 -0400

    Import BufferedReader to read text from the keyboard

--- src/array/NumberArrayReverseOrder.java
+++ src/array/NumberArrayReverseOrder.java
@@ -1,6 +1,11 @@
 package array;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class NumberArrayReverseOrder {
+    //Import BufferedReader to read text from the user input
+    public static BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) {
 
         //integer data type array that hold 10 elements
