130343fd9ac658048c631cf90c938e26660b64bf
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-04 20:10:40 -0400

    Array of numbers in reverse order

--- src/array/NumberArrayReverseOrder.java
+++ src/array/NumberArrayReverseOrder.java
@@ -19,5 +19,12 @@ public class NumberArrayReverseOrder {
             //Read the input from user and convert the string to integer and then store the value into arrNum array
             arrNum[i] = Integer.parseInt(userInput.readLine());
         }
+        //use a for loop, starting i with initial value of 9
+        //Check if i value is greater or equal 0
+        //Decrement i value by 1 after each iteration
+        for (int i = 9; i >= 0; i--) {
+            //Display the message on the screen
+            System.out.println("Reverse order array elements: " + i + " => " + arrNum[i]);
+        }
     }
 }
