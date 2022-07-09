29de7d938e374ee118d9020d6d3546758ebcc808
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-09 12:01:54 -0400

    Move into directory

--- src/java_practice/hello_world/HelloWorld.java
+++ src/java_practice/hello_world/HelloWorld.java
@@ -1,4 +1,4 @@
-package java_practice;
+package java_practice.hello_world;
 
 //The following HelloWorld class implements an application that simply prints "Hello World" to standard output
 public class HelloWorld {
