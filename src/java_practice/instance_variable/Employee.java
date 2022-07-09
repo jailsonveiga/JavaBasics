18d6fe09d3edc4597fc0c4f9f02dec8fd247a03a
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-09 16:54:34 -0400

    Employee class instance variable

---
+++ src/java_practice/instance_variable/Employee.java
@@ -0,0 +1,13 @@
+package java_practice.instance_variable;
+
+public class Employee {
+
+    // instance variable employee id
+    public int id;
+
+    // instance variable employee name
+    public String empName;
+
+    // instance variable employee age
+    public int age;
+}
