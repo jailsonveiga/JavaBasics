c767d0c5aee3178f3eacac8e7b1482d2ff28087c
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-09 11:26:54 -0400

    HelloWorld class implements an application that simply prints "Hello World" to standard output

---
+++ src/java_practice/HelloWorld.java
@@ -0,0 +1,8 @@
+package java_practice;
+
+//The following HelloWorld class implements an application that simply prints "Hello World" to standard output
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // Display the string message on the screen
+    }
+}
