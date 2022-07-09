0411fc1ceafa92966870fab4cfe83b683911ab88
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-09 19:08:09 -0400

    Local Variable

--- src/java_practice/local_variable/LocalVariable.java
+++ src/java_practice/local_variable/LocalVariable.java
@@ -2,6 +2,9 @@ package java_practice.local_variable;
 
 public class LocalVariable {
     public static void main(String[] args) {
+        VariableLocal variableLocal = new VariableLocal();
+        int sum = variableLocal.sum(10);
 
+        System.out.println("Sum of first 10 numbers -> " + sum);
     }
 }
