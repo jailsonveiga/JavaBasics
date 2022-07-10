737aea7fb021c3b72ce6a9e442dad449bdf01108
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-10 19:41:36 -0400

    char variables behave like integers

--- src/java_practice/char_behave/CharBehave.java
+++ src/java_practice/char_behave/CharBehave.java
@@ -2,6 +2,10 @@ package java_practice.char_behave;
 
 public class CharBehave {
     public static void main(String[] args) {
-
+        char ch1;
+        ch1 = 'x';
+        System.out.println("ch1 contains " + ch1);
+        ch1++; //increment ch1
+        System.out.println("ch1 is now " + ch1);
     }
 }
