aa5bc1cb95ec48a7210b873776065b1e0bf2415b
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-10 19:38:10 -0400

    Demonstrate char data type

---
+++ src/java_practice/CharCaracters/CharCharacters.java
@@ -0,0 +1,13 @@
+package java_practice.CharCaracters;
+
+public class CharCharacters {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // code for x
+        ch2 = 'y';
+
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
