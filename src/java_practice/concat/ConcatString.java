e2376bce7f107f60f0dfe2400e4c05d521ecac1e
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-13 18:17:26 -0400

    Joining twi string together

--- src/java_practice/concat/ConcatString.java
+++ src/java_practice/concat/ConcatString.java
@@ -2,6 +2,9 @@ package java_practice.concat;
 
 public class ConcatString {
     public static void main(String[] args) {
-
+        String firstString = "This is";
+        String secondString = " a concatenated string.";
+        String thirdString = firstString + secondString;
+        System.out.println(thirdString);
     }
 }
