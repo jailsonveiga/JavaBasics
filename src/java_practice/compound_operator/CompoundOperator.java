f83c8705707d2f72681e9a1ca3b315d9bd5a3120
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-13 22:23:47 -0400

    CompoundOperator

--- src/java_practice/compound_operator/CompoundOperator.java
+++ src/java_practice/compound_operator/CompoundOperator.java
@@ -2,6 +2,17 @@ package java_practice.compound_operator;
 
 public class CompoundOperator {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
 
+        a += 5;
+        b *= 4;
+        c += a * b;
+        c %= 6;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
     }
 }
