bf6e96a6aa0d04ffbc7987e481cc9b644cafa594
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-13 22:36:30 -0400

    Increament

--- src/java_practice/decreament_increament/DecreamentIncreament.java
+++ src/java_practice/decreament_increament/DecreamentIncreament.java
@@ -2,6 +2,17 @@ package java_practice.decreament_increament;
 
 public class DecreamentIncreament {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b;
+        d = a++;
+        c++;
 
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
     }
 }
