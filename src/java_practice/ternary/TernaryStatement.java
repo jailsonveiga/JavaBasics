30fd3651b6c02ca3518a825c1969400328c173a8
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-14 23:10:08 -0400

    Ternary Statement

--- src/java_practice/ternary/TernaryStatement.java
+++ src/java_practice/ternary/TernaryStatement.java
@@ -2,6 +2,16 @@ package java_practice.ternary;
 
 public class TernaryStatement {
     public static void main(String[] args) {
+        int i, j;
+        i = 10;
 
+        j = i < 0 ? -i : i; // get absolute value of 1
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + j);
+
+        i = -10;
+        j = i < 0 ? -i : i; // get absolute value of i
+        System.out.print("Absolute value of ");
+        System.out.println(i + " is " + j);
     }
 }
