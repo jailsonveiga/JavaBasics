d541b3b1e88a5ca506460465b0b1001c06cc2fbf
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-07-21 21:45:18 -0400

    Update

--- src/code_time/TemShoes.java
+++ src/code_time/TemShoes.java
@@ -1,4 +1,13 @@
 package code_time;
 
 public class TemShoes {
+    public static int result = 105;
+
+    public static void main(String[] args) {
+        byte correction = Byte.MAX_VALUE;
+        for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {
+            correction *= fahrenheit;
+            System.out.println(correction);
+        }
+    }
 }
