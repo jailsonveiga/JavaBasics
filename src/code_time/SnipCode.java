521e392b0e0d01c4a03a261cf8c59b13a7efea0c
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-07-23 21:47:26 -0400

    Executing A Snippet Of Code

--- src/code_time/SnipCode.java
+++ src/code_time/SnipCode.java
@@ -1,4 +1,10 @@
 package code_time;
 
 public class SnipCode {
+    public static int result = 45967;
+
+    public static void main(String[] args) {
+        String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
+        System.out.println(loop);
+    }
 }
