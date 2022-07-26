00da3853fc1151bcafac3a70824d42e85bcab684
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-07-25 21:49:33 -0400

    String Array In Reverse Order

--- src/code_time/StrRevOrder.java
+++ src/code_time/StrRevOrder.java
@@ -1,4 +1,19 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class StrRevOrder {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] array = new String[10];
+        for (int i = 0; i < 8; i++) {
+            array[i] = reader.readLine();
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
 }
