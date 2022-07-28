af077e0865008f607b37b4b0d63ecb813f6e6a39
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-07-27 23:09:00 -0400

    Street and Houses

--- src/code_time/StreetAndHouses.java
+++ src/code_time/StreetAndHouses.java
@@ -1,4 +1,33 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class StreetAndHouses {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] countResidents = new int[15];
+        for (int i = 0; i < 15; i++) {
+            countResidents[i] = Integer.parseInt(reader.readLine());
+        }
+
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < 15; i++) {
+            if (i % 2 == 0) {
+                even += countResidents[i];
+            } else {
+                odd += countResidents[i];
+            }
+        }
+
+        if (even > odd) {
+            System.out.println("Even-numbered houses have more residents.");
+        } else if (odd > even) {
+            System.out.println("Odd-numbered houses have more residents.");
+        }
+    }
 }
