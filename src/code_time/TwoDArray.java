dc453e8507dc778db2ea0891690897b20664e509
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-08-15 23:19:24 -0400

    2D Array

--- src/code_time/TwoDArray.java
+++ src/code_time/TwoDArray.java
@@ -1,4 +1,19 @@
 package code_time;
 
+import java.util.Scanner;
+
 public class TwoDArray {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        multiArray = new int[n][];
+        for (int i = 0; i < n; i++) {
+            int size = Integer.parseInt(scanner.nextLine());
+            multiArray[i] = new int[size];
+        }
+    }
 }
