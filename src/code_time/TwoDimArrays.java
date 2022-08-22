fddb7d0306f4d096fd33d9bf140e98711db83275
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-08-21 23:26:21 -0400

    Displaying Two-Dimensional Arrays

--- src/code_time/TwoDimArrays.java
+++ src/code_time/TwoDimArrays.java
@@ -1,4 +1,19 @@
 package code_time;
 
+import java.util.Arrays;
+
+
 public class TwoDimArrays {
+    public static String[][] strings = new String[][]{
+            {"🙂", "🙂", "🙂", "🙂"},
+            {"🙃", "🙃", "🙃", "🙃"},
+            {"😋", "😋", "😋", "😋"},
+            {"😎", "😎", "😎", "😎"}
+    };
+    public static int[][] ints = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(strings));
+        System.out.println(Arrays.deepToString(ints));
+    }
 }
