ab56d1c48ce411a943706e33a739a1496ce731ee
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-08-23 23:28:09 -0400

    Comparing Two Dimensional Arrays

--- src/code_time/ComTwoDimArrays.java
+++ src/code_time/ComTwoDimArrays.java
@@ -1,4 +1,12 @@
 package code_time;
 
+import java.util.Arrays;
+
 public class ComTwoDimArrays {
+    public static int[][] arrayFirst = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+    public static int[][] arraySecond = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepEquals(arrayFirst, arraySecond));
+    }
 }
