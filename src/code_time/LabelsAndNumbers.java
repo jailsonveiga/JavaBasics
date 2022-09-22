fd472270a88df51dca14e16436e5b84178030101
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-21 21:00:59 -0400

    Labels And Numbers

--- src/code_time/LabelsAndNumbers.java
+++ src/code_time/LabelsAndNumbers.java
@@ -1,4 +1,22 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class LabelsAndNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number == 0) {
+            System.out.println("Zero");
+        } else if (number > 0 && number % 2 == 0) {
+            System.out.println("Positive even number");
+        } else if (number > 0 && number % 2 != 0) {
+            System.out.println("Positive odd number");
+        } else if (number < 0 && number % 2 == 0) {
+            System.out.println("Negative even number");
+        } else if (number < 0 && number % 2 != 0) {
+            System.out.println("Negative odd number");
+        }
+    }
 }
