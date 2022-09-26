36996f47e732998a413fe5361098a58d09f53dd6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-25 21:09:42 -0400

    Ten Numbers

--- src/code_time/TenNum.java
+++ src/code_time/TenNum.java
@@ -1,4 +1,11 @@
 package code_time;
 
 public class TenNum {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
