ec81479a05e8d28eff54ab1228a1556ff4eeee95
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-26 21:10:59 -0400

    From Ten To One

--- src/code_time/FromTenToOne.java
+++ src/code_time/FromTenToOne.java
@@ -1,4 +1,11 @@
 package code_time;
 
 public class FromTenToOne {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
 }
