db66abf3d1ddfaaadb995d169d18efd5af4579fe
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-27 21:12:44 -0400

    You Can Not Have Too Much Of A Good Thing

--- src/code_time/ToMuch.java
+++ src/code_time/ToMuch.java
@@ -1,4 +1,17 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ToMuch {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        int number = Integer.parseInt(reader.readLine());
+
+        while (number > 0) {
+            System.out.println(string);
+            number--;
+        }
+    }
 }
