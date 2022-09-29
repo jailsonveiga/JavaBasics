73992b8988099b05a098401ad566d09ae6a8047f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-28 21:14:21 -0400

    Seeing Dollars In Your Future

--- src/code_time/DollarSign.java
+++ src/code_time/DollarSign.java
@@ -1,4 +1,12 @@
 package code_time;
 
 public class DollarSign {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 10) {
+            System.out.println("$$$$$$$$$$");
+            i++;
+        }
+    }
 }
