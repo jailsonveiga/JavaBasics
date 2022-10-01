d022a3f13dfa74a49ad72a13775fe7a5a7f32d13
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-30 21:16:58 -0400

    Even Number

--- src/code_time/EvenNum.java
+++ src/code_time/EvenNum.java
@@ -1,4 +1,9 @@
 package code_time;
 
 public class EvenNum {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
 }
