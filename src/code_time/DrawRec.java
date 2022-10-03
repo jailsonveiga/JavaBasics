cd960e3964198e94aa03da302199ef717e26ad74
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-02 21:19:24 -0400

    Drawing A Rectangle

--- src/code_time/DrawRec.java
+++ src/code_time/DrawRec.java
@@ -1,4 +1,20 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DrawRec {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(reader.readLine());
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print('8');
+            }
+
+            System.out.println();
+        }
+    }
 }
