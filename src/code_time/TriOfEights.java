870c985126ab2512467e4a8f60f65d4914b83965
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-03 21:20:58 -0400

    Triangle Of Eights

--- src/code_time/TriOfEights.java
+++ src/code_time/TriOfEights.java
@@ -1,4 +1,13 @@
 package code_time;
 
 public class TriOfEights {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print('8');
+            }
+
+            System.out.println();
+        }
+    }
 }
