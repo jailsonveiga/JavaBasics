f98e515b9dfa5a39e2bb742ac6b07fa21eeed004
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-04 21:22:22 -0400

    Drawing Lines

--- src/code_time/DrawLines.java
+++ src/code_time/DrawLines.java
@@ -1,4 +1,14 @@
 package code_time;
 
 public class DrawLines {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print('8');
+        }
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println('8');
+        }
+    }
 }
