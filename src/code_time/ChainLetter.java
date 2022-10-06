2f0de8f722c0face1a1f891a1c252490bc571171
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-05 21:23:46 -0400

    Chain Letter

--- src/code_time/ChainLetter.java
+++ src/code_time/ChainLetter.java
@@ -1,4 +1,16 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ChainLetter {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String text = name + " loves me.";
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(text);
+        }
+    }
 }
