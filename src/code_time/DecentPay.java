b6c3171ec512bf0a980e356e3c80e8f9c8178af0
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-06 21:25:15 -0400

    Decent Pay

--- src/code_time/DecentPay.java
+++ src/code_time/DecentPay.java
@@ -1,4 +1,9 @@
 package code_time;
 
 public class DecentPay {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I will never work for peanuts. Amigo");
+        }
+    }
 }
