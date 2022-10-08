b2244c6bbbabad635b7251feb4036dd01c6ffc63
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-07 21:27:02 -0400

    Somewhere In The Middle

--- src/code_time/SomWhereInTheMiddle.java
+++ src/code_time/SomWhereInTheMiddle.java
@@ -1,4 +1,21 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class SomWhereInTheMiddle {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+        int number3 = Integer.parseInt(reader.readLine());
+
+        if ((number1 >= number2 && number1 <= number3) || (number1 <= number2 && number1 >= number3)) {
+            System.out.println(number1);
+        } else if ((number2 >= number1 && number2 <= number3) || (number2 <= number1 && number2 >= number3)) {
+            System.out.println(number2);
+        } else {
+            System.out.println(number3);
+        }
+    }
 }
