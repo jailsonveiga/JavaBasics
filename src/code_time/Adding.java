63a3ddfa7dc79d59e61eacf21c687422a823f781
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-08 21:28:25 -0400

    Adding

--- src/code_time/Adding.java
+++ src/code_time/Adding.java
@@ -1,4 +1,21 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Adding {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        while (true) {
+            int number = Integer.parseInt(reader.readLine());
+            sum += number;
+
+            if (number == -1) {
+                break;
+            }
+        }
+        System.out.println(sum);
+    }
 }
