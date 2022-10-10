bb2c315e13281095055f2a216343a12c1f2c4a9c
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-10 13:27:52 -0400

    Display Few Text On The Screen

--- src/code_time/MoreText.java
+++ src/code_time/MoreText.java
@@ -1,4 +1,13 @@
 package code_time;
 
 public class MoreText {
+    public static void main(String[] args) {
+        String text = "Jay is very smart";
+
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+    }
 }
