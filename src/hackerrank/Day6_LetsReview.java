4c2955e32908b2b74e7dc2231d75f56aba703962
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-12 12:28:22 -0400

    30 Days Of Code "HackerRank" Day 6 - Let's Review

--- src/hackerrank/Day6_LetsReview.java
+++ src/hackerrank/Day6_LetsReview.java
@@ -1,4 +1,41 @@
 package hackerrank;
 
+import java.util.*;
+
 public class Day6_LetsReview {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        in.nextLine();
+
+        for (int i = 0; i < N; i++)
+        {
+            String string = in.nextLine();
+            char[] charArray = string.toCharArray();
+
+            for (int j = 0; j < charArray.length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.print(" ");
+
+            for (int j = 0; j < charArray.length; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    System.out.print(charArray[j]);
+                }
+            }
+
+            System.out.println();
+        }
+
+        in.close();
+    }
 }
