a2eb9d027bd372d77506042aaaa7a235c6208331
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-13 20:33:12 -0400

    30 Days Of Code "HackerRank" Day 10 - Binary Numbers

---
+++ src/hackerrank/Day10_BinaryNumbers.java
@@ -0,0 +1,27 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day10_BinaryNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int sum = 0, max = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                sum++;
+                if (sum > max) {
+                    max = sum;
+                }
+            } else {
+                sum = 0;
+            }
+            n = n / 2;
+        }
+        System.out.println(max);
+    }
+}
