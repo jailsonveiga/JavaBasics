d368882f96b7413e1f23e2235f2ec4d088ac07a6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-17 19:22:12 -0400

    30 Days Of Code "HackerRank" Day 13 - Abstract Classes

--- src/hackerrank/Day13_Abstract_Classes.java
+++ src/hackerrank/Day13_Abstract_Classes.java
@@ -1,4 +1,43 @@
 package hackerrank;
+import java.io.*;
+import java.util.*;
 
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+class MyBook extends Book{
+    int price;
+
+    MyBook(String title,String author,int price ){
+        super(title,author);
+        this.price=price;
+    }
+    void display()
+    {
+        System.out.println("Title: "+title);
+        System.out.println("Author: "+author);
+        System.out.println("Price: "+price);
+    }
+}
 public class Day13_Abstract_Classes {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
 }
