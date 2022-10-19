28c5d00637fc898533ef923a0cf4d03b123cd3ab
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-18 20:40:47 -0400

    30 Days Of Code "HackerRank" Day 19 - Interfaces

--- src/hackerrank/Day19_Interfaces.java
+++ src/hackerrank/Day19_Interfaces.java
@@ -1,5 +1,31 @@
 package hackerrank;
 
-public class Day19_Interfaces {
+import java.util.Scanner;
 
+class Calculator implements AdvancedArithmetic
+{
+
+    public int divisorSum(int n)
+    {
+        int sum=0;
+        for(int i=1 ; i<=n;i++)
+        {
+            if(n % i ==0)
+                sum+=i;
+        }
+        return sum;
+    }
+}
+
+public class Day19_Interfaces {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new Calculator();
+        int sum = ((Calculator) myCalculator).divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
 }
