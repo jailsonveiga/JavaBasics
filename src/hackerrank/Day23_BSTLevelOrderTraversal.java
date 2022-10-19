04449f6c49bf9bb71dfa041fa88afa90e61d7493
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-19 11:43:46 -0400

    30 Days Of Code "HackerRank" Day 23 - BST Level-Order Traversal

--- src/hackerrank/Day23_BSTLevelOrderTraversal.java
+++ src/hackerrank/Day23_BSTLevelOrderTraversal.java
@@ -1,4 +1,63 @@
 package hackerrank;
 
-public class Day23_BSTLevelOrderTraversal {
+import java.util.*;
+
+//class Node{
+//    Node left,right;
+//    int data;
+//    Node(int data){
+//        this.data=data;
+//        left=right=null;
+//    }
+//}
+class Day23_BSTLevelOrderTraversal{
+
+    static LinkedList<Integer> queue = new LinkedList();
+
+    static void levelOrder(Node root){
+        LinkedList<Node> treeQueue = new LinkedList();
+        treeQueue.add(root);
+        while(treeQueue.peek() != null) {
+            Node toprint = treeQueue.remove();
+            System.out.print(toprint.data);
+            if(toprint.left != null) {
+                treeQueue.add(toprint.left);
+            }
+            if(toprint.right != null) {
+                treeQueue.add(toprint.right);
+            }
+            if(treeQueue.peek() != null) {
+                System.out.print(" ");
+            }
+        }
+
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        levelOrder(root);
+    }
 }
