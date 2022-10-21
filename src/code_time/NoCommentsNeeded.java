fbc943a0a6ef714681e2babf2d6d6abba71f5242
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-21 13:35:39 -0400

    Initialize a Variable

--- src/code_time/NoCommentsNeeded.java
+++ src/code_time/NoCommentsNeeded.java
@@ -1,4 +1,17 @@
 package code_time;
 
 public class NoCommentsNeeded {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 12;
+
+        //y = x * y;
+        y = x + y;
+
+        x = y - x;
+        y = y - x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
