b296b90ce544c642e0ea5d418e869abfe49a80fa
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-22 13:38:43 -0400

    Initial Commit

--- src/code_time/TwoPlusThreeEqualFive.java
+++ src/code_time/TwoPlusThreeEqualFive.java
@@ -1,4 +1,19 @@
 package code_time;
 
 public class TwoPlusThreeEqualFive {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.print("two");
+        System.out.print(b);
+        System.out.print(" plus ");
+        System.out.print(" minus ");
+        System.out.print(a);
+        System.out.print("three");
+        System.out.print(" is equal to ");
+        System.out.print(" equals ");
+        System.out.print("five");
+        System.out.print(a + b);
+    }
 }
