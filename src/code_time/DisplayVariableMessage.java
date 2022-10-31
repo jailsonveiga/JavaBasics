e2fa353730b3ea38cd66eacf6f632fd698a7465f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-10-31 13:52:03 -0400

    Display Variable value

---
+++ src/code_time/DisplayVariableMessage.java
@@ -0,0 +1,9 @@
+package code_time;
+
+public class DisplayVariableMessage {
+    public static void main(String[] args) {
+        String name = "Amigo";
+
+        System.out.print(name);
+    }
+}
