f8be9c10185f0d4ed92f0787611f64454a743f01
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-01 13:54:49 -0400

    Display The Square Of A Number

--- src/code_time/SquareOfANumber.java
+++ src/code_time/SquareOfANumber.java
@@ -1,4 +1,9 @@
 package code_time;
 
 public class SquareOfANumber {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        System.out.println(number * number);
+    }
 }
