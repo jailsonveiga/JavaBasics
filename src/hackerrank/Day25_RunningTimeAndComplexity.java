86aac742c369937a883c04313afd4837f97e8562
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-02 20:26:26 -0400

    Update

--- src/hackerrank/Day25_RunningTimeAndComplexity.java
+++ src/hackerrank/Day25_RunningTimeAndComplexity.java
@@ -1,4 +1,46 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 public class Day25_RunningTimeAndComplexity {
+    public static void isPrime(int n){
+
+        if(n==0 || n== 1){
+            System.out.println("Not prime");
+        }
+        else if(n==2){
+            System.out.println("Prime");
+        }
+        else{
+            boolean result = true;
+            for(int i= 2; i<= n/2; i++){
+                if(n%i == 0){
+                    result = false;
+                    break;
+                }
+            }
+
+            if(result == true){
+                System.out.println("Prime");
+            }
+            else{
+                System.out.println("Not prime");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while(T-- >0){
+
+            int n = sc.nextInt();
+            isPrime(n);
+        }
+
+        sc.close();
+    }
 }
