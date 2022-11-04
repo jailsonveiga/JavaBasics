2faa558ddb736bc6658e9fee38a824db464ed9db
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-04 18:21:08 -0400

    Display The Variable name Value

--- src/code_time/VariableDisplayValue.java
+++ src/code_time/VariableDisplayValue.java
@@ -1,4 +1,9 @@
 package code_time;
 
 public class VariableDisplayValue {
+    public static void main(String[] args) {
+        String name = "Amigo";
+        String text = "Hello, " + name + "!";
+        System.out.println(text);
+    }
 }
