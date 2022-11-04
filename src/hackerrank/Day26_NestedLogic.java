58237c4c18f668445c02c063380b0e955ebf781e
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-03 20:30:23 -0400

    Update

--- src/hackerrank/Day26_NestedLogic.java
+++ src/hackerrank/Day26_NestedLogic.java
@@ -1,4 +1,38 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 public class Day26_NestedLogic {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int fine;
+
+        Scanner scan = new Scanner(System.in);
+
+        int returnDay = scan.nextInt();
+        int returnMonth = scan.nextInt();
+        int returnYear = scan.nextInt();
+
+        int dueDay = scan.nextInt();
+        int dueMonth = scan.nextInt();
+        int dueYear = scan.nextInt();
+
+        scan.close();
+
+        if(returnYear <= dueYear){
+            if(returnMonth <= dueMonth){
+                if(returnDay <= dueDay){
+                    fine = 0;
+                }else{
+                    fine = (returnDay - dueDay) * 15;
+                }
+            }else{
+                fine = (returnMonth - dueMonth) * 500;
+            }
+        }else{
+            fine = 10000;
+        }
+
+        System.out.println(fine);
+    }
 }
