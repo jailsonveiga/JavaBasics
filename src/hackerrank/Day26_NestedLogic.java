7b9af4f6c1f0adc9b16a878acb8873d2c780f452
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-03 20:31:56 -0400

    Update

--- src/hackerrank/Day26_NestedLogic.java
+++ src/hackerrank/Day26_NestedLogic.java
@@ -3,36 +3,37 @@ package hackerrank;
 import java.util.Scanner;
 
 public class Day26_NestedLogic {
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        int fine;
+    public static int fine(int[] returnData , int[] dueData ){
 
-        Scanner scan = new Scanner(System.in);
-
-        int returnDay = scan.nextInt();
-        int returnMonth = scan.nextInt();
-        int returnYear = scan.nextInt();
-
-        int dueDay = scan.nextInt();
-        int dueMonth = scan.nextInt();
-        int dueYear = scan.nextInt();
-
-        scan.close();
-
-        if(returnYear <= dueYear){
-            if(returnMonth <= dueMonth){
-                if(returnDay <= dueDay){
-                    fine = 0;
-                }else{
-                    fine = (returnDay - dueDay) * 15;
-                }
-            }else{
-                fine = (returnMonth - dueMonth) * 500;
-            }
-        }else{
+        int fine=0;
+        if(returnData[2] > dueData[2]){
             fine = 10000;
+            return fine;
+        }else if(returnData[2] == dueData[2] && returnData[1] > dueData[1]){
+            fine = (returnData[1] - dueData[1])*500;
+            return fine;
+        }else if(returnData[2] == dueData[2] && returnData[1] == dueData[1] && returnData[0] > dueData[0]){
+            fine=(returnData[0] - dueData[0]) * 15;
+            return fine;
+        }else
+            return fine;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int returnData[] = new int [3];
+        for(int i=0;i<returnData.length;i++){
+            returnData[i] = sc.nextInt();
         }
 
+        int dueData[] = new int [3];
+        for(int i=0;i<dueData.length;i++){
+            dueData[i] = sc.nextInt();
+        }
+        int fine=fine(returnData, dueData);
         System.out.println(fine);
+
     }
 }
