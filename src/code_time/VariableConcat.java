859a0a653233b39a5e34e2ab9239890eb03bb5ec
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-05 18:22:53 -0400

    Concat Variable Values

--- src/code_time/VariableConcat.java
+++ src/code_time/VariableConcat.java
@@ -1,4 +1,10 @@
 package code_time;
 
 public class VariableConcat {
+    public static void main(String[] args) {
+        String string1 = "Coding";
+        String string2 = "in";
+        String string3 = "Java";
+        System.out.println(string1 + " " + string2 + " " + string3);
+    }
 }
