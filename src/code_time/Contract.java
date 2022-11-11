df5f5c05da3cfd614f78c788451910ab7b785834
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-11 18:27:49 -0500

    Contract

--- src/code_time/Contract.java
+++ src/code_time/Contract.java
@@ -1,4 +1,16 @@
 package code_time;
 
 public class Contract {
+    public static void main(String[] args) {
+        //write your code here
+        System.out.println("My name is Amigo.");
+        System.out.println();
+        System.out.println("I agree to wages of $5000/month in the first year.");
+        System.out.println("I agree to wages of $5500/month in the second year.");
+        System.out.println("I agree to wages of $7000/month in the third year.");
+        System.out.println("I agree to wages of $8000/month in the fourth year.");
+        System.out.println("I agree to wages of $10000/month in the fifth year.");
+        System.out.println();
+        System.out.println("Kiss my shiny metal rear actuator!");
+    }
 }
