6d3c31b121df08f9d5bea8cca3b7dd5def76a8c0
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-12 18:29:55 -0500

    How To Take Over The Word

--- src/code_time/TakeOverTheWord.java
+++ src/code_time/TakeOverTheWord.java
@@ -1,4 +1,13 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TakeOverTheWord {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String years = reader.readLine();
+        System.out.println(name + " will take over the world in " + years + " years. Mwa-ha-ha!");
+    }
 }
