32fda77af8337b3a9bb575c4fe328cdd0c6052b7
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-13 20:23:39 -0500

    Predictions

--- src/code_time/Predictions.java
+++ src/code_time/Predictions.java
@@ -1,4 +1,16 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Predictions {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String money = reader.readLine();
+
+        String years = reader.readLine();
+
+        System.out.println(name + " will receive " + money + " in " + years + " years.");
+    }
 }
