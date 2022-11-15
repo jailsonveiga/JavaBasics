dc94d62a206476095f35d2beac02ad1925f548a6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-14 20:25:35 -0500

    Pure Love

--- src/code_time/DeepAndPureLove.java
+++ src/code_time/DeepAndPureLove.java
@@ -1,4 +1,15 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DeepAndPureLove {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name1 = reader.readLine();
+        String name2 = reader.readLine();
+        String name3 = reader.readLine();
+
+        System.out.println(name1 + " + " + name2 + " + " + name3 + " = Pure love. Ooo la-la!");
+    }
 }
