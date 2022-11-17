e7423130602a25e107d67220b3ce2b2554253004
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-16 20:28:28 -0500

    Person Age

--- src/code_time/PersonAge.java
+++ src/code_time/PersonAge.java
@@ -1,4 +1,19 @@
 package code_time;
 
 public class PersonAge {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println("Age: " + person.age);
+        person.adjustAge(person.age);
+        System.out.println("Adjusted age: " + person.age);
+    }
+
+    public static class Person {
+        public int age = 20;
+
+        public void adjustAge(int age) {
+            this.age = age + 20;
+            System.out.println("The age in adjustAge() is: " + age);
+        }
+    }
 }
