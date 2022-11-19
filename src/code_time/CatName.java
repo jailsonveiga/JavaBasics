93eaa7b2bd487436ce2c1adbaf89d326b5024bb2
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-18 20:32:06 -0500

    Cat Name

--- src/code_time/CatName.java
+++ src/code_time/CatName.java
@@ -1,4 +1,17 @@
 package code_time;
 
+import variable_visibility.Cat;
+
 public class CatName {
+    private String name = "nameless cat";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Charlie");
+        System.out.println(cat.name);
+    }
 }
