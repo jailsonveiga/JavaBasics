6d74759b9b946a0a3141f3861bc4a0d8ee5fa4d1
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-18 20:30:54 -0500

    Price Of Apples

--- src/code_time/PriceOfApples.java
+++ src/code_time/PriceOfApples.java
@@ -1,4 +1,19 @@
 package code_time;
 
 public class PriceOfApples {
+    public static void main(String[] args) {
+        Apple apple = new Apple();
+        Apple.addPrice(50);
+        Apple apple2 = new Apple();
+        Apple.addPrice(100);
+        System.out.println("Apple price " + Apple.applePrice);
+    }
+
+    public static class Apple {
+        public static int applePrice = 0;
+
+        public static void addPrice(int applesPrice) {
+            Apple.applePrice = Apple.applePrice + applesPrice;
+        }
+    }
 }
