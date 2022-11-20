bd8f69151da5dbf85c15ef4fb3365620f6456541
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-19 20:38:05 -0500

    Update

--- src/code_time/CatRegister.java
+++ src/code_time/CatRegister.java
@@ -1,4 +1,13 @@
 package code_time;
 
 public class CatRegister {
+        private static int catCount = 0;
+
+        public static void addNewCat() {
+            catCount++;
+        }
+
+        public static void main(String[] args) {
+
+        }
 }
