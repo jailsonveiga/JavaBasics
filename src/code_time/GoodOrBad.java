69b29e5f8abb2663f2e6bfa1501e5dab377ce638
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-29 20:42:25 -0500

    Less Or Greater

--- src/code_time/GoodOrBad.java
+++ src/code_time/GoodOrBad.java
@@ -1,4 +1,17 @@
 package code_time;
 
 public class GoodOrBad {
+    public static void main(String[] args) {
+        compare(3);
+        compare(6);
+        compare(5);
+    }
+
+    public static void compare(int a) {
+        if (a < 5) {
+            System.out.println("The number is less than 5");
+        } else {
+            System.out.println("The number is equal or greater than 5");
+        }
+    }
 }
