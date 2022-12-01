edddcb1b0cd979d1bbef0c4a48a52bd991fbaf2a
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-11-30 20:44:19 -0500

    Check The Interval

--- src/code_time/Interval.java
+++ src/code_time/Interval.java
@@ -1,4 +1,24 @@
 package code_time;
 
 public class Interval {
+    public static void main(String[] args) {
+        checkInterval(60);
+        checkInterval(112);
+        checkInterval(10);
+    }
+
+    public static void checkInterval(int a) {
+        String in = "The number " + a + " is in the interval.";
+        String out = "The number " + a + " is not in the interval.";
+
+        if (a > 50) {
+            if (a < 100) {
+                System.out.println(in);
+            } else {
+                System.out.println(out);
+            }
+        } else {
+            System.out.println(out);
+        }
+    }
 }
