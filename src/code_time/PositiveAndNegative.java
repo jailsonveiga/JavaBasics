548daa896aa0b32127dee4625e46d601bb8a3838
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-01 20:46:23 -0500

    Positive And Negative Number

--- src/code_time/PositiveAndNegative.java
+++ src/code_time/PositiveAndNegative.java
@@ -1,4 +1,18 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PositiveAndNegative {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+
+        if (number > 0) {
+            number = number * 2;
+        } else if (number < 0) {
+            number = number + 1;
+        }
+        System.out.println(number);
+    }
 }
