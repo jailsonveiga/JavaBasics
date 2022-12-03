7fb4f93ca9a530ceb92a16193aa4e61154d1c27a
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-02 20:48:15 -0500

    Day Of The Week

--- src/code_time/DayOfTheWeek.java
+++ src/code_time/DayOfTheWeek.java
@@ -1,4 +1,36 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DayOfTheWeek {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int day = Integer.parseInt(reader.readLine());
+        String result = "No such day of the week";
+
+        if (day == 1) {
+            result = "Monday";
+        }
+        if (day == 2) {
+            result = "Tuesday";
+        }
+        if (day == 3) {
+            result = "Wednesday";
+        }
+        if (day == 4) {
+            result = "Thursday";
+        }
+        if (day == 5) {
+            result = "Friday";
+        }
+        if (day == 6) {
+            result = "Saturday";
+        }
+        if (day == 7) {
+            result = "Sunday";
+        }
+
+        System.out.println(result);
+    }
 }
