5fb6ed90f51d494c97e3d2a24bec1ca8e0113f04
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-03 20:49:38 -0500

    Initial Commit

--- src/code_time/MinOfTwoNum.java
+++ src/code_time/MinOfTwoNum.java
@@ -1,4 +1,20 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class MinOfTwoNum {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number1 = Integer.parseInt(reader.readLine());
+        int number2 = Integer.parseInt(reader.readLine());
+
+        int min;
+        if (number1 < number2) {
+            min = number1;
+        } else {
+            min = number2;
+        }
+        System.out.println(min);
+    }
 }
