02838968d1bd004c25f39d4a72051accc26b946f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-04 20:53:11 -0500

    Eighteen Plus

--- src/code_time/EighteenPlus.java
+++ src/code_time/EighteenPlus.java
@@ -1,4 +1,16 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class EighteenPlus {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        if (age < 18) {
+            System.out.println("Grow up a little more");
+        }
+    }
 }
