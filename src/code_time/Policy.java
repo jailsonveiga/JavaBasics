0f04827551b2c1159afc79cc2fa9d79bb4f1a6bb
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-05 20:54:42 -0500

    Bouncer Policy

--- src/code_time/Policy.java
+++ src/code_time/Policy.java
@@ -1,4 +1,16 @@
 package code_time;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Policy {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        if (age > 20) {
+            System.out.println("18 is old enough");
+        }
+    }
 }
