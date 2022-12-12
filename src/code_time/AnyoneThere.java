e7797ab0a531ca27d10f577b66753959641a5a98
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-12 17:18:22 -0500

    Anyone There

--- src/code_time/AnyoneThere.java
+++ src/code_time/AnyoneThere.java
@@ -1,4 +1,15 @@
 package code_time;
 
+import java.util.Arrays;
+
 public class AnyoneThere {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        int[] copy = Arrays.copyOf(array, array.length);
+        Arrays.sort(copy);
+        int index = Arrays.binarySearch(copy, element);
+        System.out.println(index >= 0);
+    }
 }
