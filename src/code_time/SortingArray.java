5ec7e92f164a5096fff88cf15d74f359798d5e9f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-12 17:16:33 -0500

    Sorting Array

--- src/code_time/SortingArray.java
+++ src/code_time/SortingArray.java
@@ -1,4 +1,12 @@
 package code_time;
 
+import java.util.Arrays;
+
 public class SortingArray {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
