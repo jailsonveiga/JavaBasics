f30596e63c02b325fd1f331d3f450654cfe486e7
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-14 16:21:19 -0500

    Favorite Dish

--- src/code_time/FavDish.java
+++ src/code_time/FavDish.java
@@ -1,4 +1,17 @@
 package code_time;
 
 public class FavDish {
+    public static void main(String[] args) {
+        String firstName = "Jon";
+        String lastName = "Jay";
+        String favoriteDish = "Spaghetti";
+        printPersonInfo(firstName, lastName, favoriteDish);
+    }
+
+    public static void printPersonInfo(String name, String surname, String meal) {
+        System.out.println("Short dossier:");
+        System.out.println("First name: " + name);
+        System.out.println("Last name: " + surname);
+        System.out.println("Favorite dish: " + meal);
+    }
 }
