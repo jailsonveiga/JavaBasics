61c2b62b0b29960b8714c4870f7f61732f349603
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-16 22:26:16 -0500

    Welcome and greeting

--- src/code_time/Welcome.java
+++ src/code_time/Welcome.java
@@ -1,4 +1,15 @@
 package code_time;
 
 public class Welcome {
+    public static void main(String[] args) {
+        signIn("Jay");
+    }
+
+    public static void signIn(String username) {
+        if (username.equals("user")) {
+            return;
+        }
+        System.out.println("Welcome, " + username);
+        System.out.println("We've really missed you, " + username);
+    }
 }
