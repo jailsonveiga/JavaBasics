1f2cb7673c39a2a59df98360cc0945b69dea2b81
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-17 21:05:01 -0500

    Add an instance variables, representing the state of a Dog object

--- src/code_time/Dog.java
+++ src/code_time/Dog.java
@@ -1,4 +1,11 @@
 package code_time;
 
+// The Dog class represents a dog object, with a name and an age
 public class Dog {
+    //There are instance variables, representing the state of a Dog object.
+    // They are private, so they can only be accessed directly within the Dog class
+
+    private String name;
+    private int age;
+
 }
