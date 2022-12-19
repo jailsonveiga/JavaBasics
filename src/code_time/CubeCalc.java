73f1a3d411e3ef4865cede26b630880973125ff2
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-18 21:33:36 -0500

    Cube Calculator

--- src/code_time/CubeCalc.java
+++ src/code_time/CubeCalc.java
@@ -1,4 +1,12 @@
 package code_time;
 
 public class CubeCalc {
+    public static void main(String[] args) {
+       System.out.println(cube(5));
+    }
+
+    public static long cube(long a) {
+        return a * a * a;
+    }
+
 }
