79188fa0aed51b47cfa0de327d512ea4dfb24a26
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-18 21:31:08 -0500

    Update

--- src/code_time/Main.java
+++ src/code_time/Main.java
@@ -3,10 +3,10 @@ package code_time;
 
 public class Main{
         public static void main(String[] args) {
-            // here we create a new Main object using the constructor
+            // here we create a new Dog object using the constructor
             Dog d = new Dog("Neru", 3);
 
-            // We use the getter method to retrieve the name and age of the Main
+            // We use the getter method to retrieve the name and age of the Dog
             System.out.println(d.getName()); // prints "Neru"
             System.out.println(d.getAge()); // prints 3
         }
