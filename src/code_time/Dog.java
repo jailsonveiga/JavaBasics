2315cd5fe2256207227ce116c3d3d8ffeb4deadd
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-18 21:31:03 -0500

    Update

--- src/code_time/Dog.java
+++ src/code_time/Dog.java
@@ -1,9 +1,9 @@
 package code_time;
 
-// The Main class represents a dog object, with a name and an age
+// The Dog class represents a dog object, with a name and an age
 public class Dog {
-    //There are instance variables, representing the state of a Main object.
-    // They are private, so they can only be accessed directly within the Main class
+    //There are instance variables, representing the state of a Dog object.
+    // They are private, so they can only be accessed directly within the Dog class
     private String name;
     private int age;
 
