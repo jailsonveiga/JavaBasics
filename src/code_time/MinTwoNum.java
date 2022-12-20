7d7a5756e70af223f6866db618e2e18476360eee
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-19 21:35:56 -0500

    Minimum of two numbers

--- src/code_time/MinTwoNum.java
+++ src/code_time/MinTwoNum.java
@@ -1,4 +1,17 @@
 package code_time;
 
 public class MinTwoNum {
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(12, 33));
+        System.out.println(min(-20, 0));
+        System.out.println(min(-10, -20));
+    }
 }
