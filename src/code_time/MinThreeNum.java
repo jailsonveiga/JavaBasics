0098f5b2b39edfcce443b7a2d1526bf15938a0a6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-20 21:37:35 -0500

    Minimum of three numbers

--- src/code_time/MinThreeNum.java
+++ src/code_time/MinThreeNum.java
@@ -1,4 +1,20 @@
 package code_time;
 
 public class MinThreeNum {
+    public static int min(int a, int b, int c) {
+        if (a <= b && a <= c) {
+            return a;
+        } else if (b <= a && b <= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(-1, -2, -3));
+        System.out.println(min(3, 5, 3));
+        System.out.println(min(5, 5, 10));
+    }
 }
