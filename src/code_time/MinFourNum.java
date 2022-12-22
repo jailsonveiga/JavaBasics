47c24ec013ebd0a1a0b515003bced9d52d90e01b
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-21 21:39:05 -0500

    Minimum of four numbers

--- src/code_time/MinFourNum.java
+++ src/code_time/MinFourNum.java
@@ -1,4 +1,23 @@
 package code_time;
 
 public class MinFourNum {
+    public static int min(int a, int b, int c, int d) {
+        return min(min(a, b), min(c, d));
+    }
+
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
 }
