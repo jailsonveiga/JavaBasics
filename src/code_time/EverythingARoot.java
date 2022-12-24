0b575698f7a19564c04c12abae2ce8ffab0a6ab9
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-23 19:53:31 -0500

    Everything Has a Root

--- src/code_time/EverythingARoot.java
+++ src/code_time/EverythingARoot.java
@@ -1,4 +1,17 @@
 package code_time;
 
 public class EverythingARoot {
+    public static void main(String[] args) {
+        int[] array = {15, 64, 9, 51, 42};
+        printSqrt(array);
+    }
+
+    public static void printSqrt(int[] array) {
+        String elementSqrtString = "The square root of ";
+        for (int i = 0; i < array.length; i++) {
+            int element = array[i];
+            double elementSqrt = Math.sqrt(element);
+            System.out.println(elementSqrtString + element + " is " + elementSqrt);
+        }
+    }
 }
