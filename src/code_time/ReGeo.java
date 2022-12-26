0369e240bf4a52ac07c987cd508f8d1f6e23abd6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-25 19:59:14 -0500

    Remembering Geometry

--- src/code_time/ReGeo.java
+++ src/code_time/ReGeo.java
@@ -1,4 +1,12 @@
 package code_time;
 
 public class ReGeo {
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("The area of the circle is: " + pi * radius * radius);
+        System.out.println("The perimeter of the circle is: " + pi * diameter);
+    }
 }
