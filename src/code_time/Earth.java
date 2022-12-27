6e30f3266ea1aea3d84ae24ab39ea4c1bece3300
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-26 20:05:14 -0500

    Everything you need to know about Earth

--- src/code_time/Earth.java
+++ src/code_time/Earth.java
@@ -1,4 +1,15 @@
 package code_time;
 
 public class Earth {
+    public static void main(String[] args) {
+        final String NAME = "Earth";
+        final double SQUARE = 510_100_000;
+        final long POPULATION = 7_594_000_000L;
+        final long EQUATOR_LENGTH = 40_075_696;
+
+        System.out.println(NAME);
+        System.out.println(SQUARE);
+        System.out.println(POPULATION);
+        System.out.println(EQUATOR_LENGTH);
+    }
 }
