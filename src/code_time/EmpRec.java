3bbc703a75aa562f1b32ca3ae072c8cd10271f53
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-12-27 20:09:24 -0500

    Employee Records

--- src/code_time/EmpRec.java
+++ src/code_time/EmpRec.java
@@ -1,4 +1,21 @@
 package code_time;
 
 public class EmpRec {
+    public static String name = "Amigo";
+    public static String position = "Java developer";
+    public static int salary = 10_000;
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(name);
+        System.out.println(position);
+        System.out.println(salary);
+    }
 }
