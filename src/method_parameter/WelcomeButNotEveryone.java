61cd7310ecb01391832f9b9d8362169994e7b55f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-07 20:41:29 -0500

    Update

--- src/method_parameter/WelcomeButNotEveryone.java
+++ src/method_parameter/WelcomeButNotEveryone.java
@@ -2,6 +2,13 @@ package method_parameter;
 
 public class WelcomeButNotEveryone {
     public static void main(String[] args) {
-
+       signIn("Jay");
+    }
+    public static void signIn(String username) {
+        if (username.equals("user")) {
+            return;
+        }
+        System.out.println("Welcome, " + username);
+        System.out.println("We've really missed you, " + username);
     }
 }
