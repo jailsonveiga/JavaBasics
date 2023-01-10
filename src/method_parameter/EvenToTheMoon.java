647ae9487e868e8c47ae59115a9733a180507ee4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-09 20:46:52 -0500

    Update

--- src/method_parameter/EvenToTheMoon.java
+++ src/method_parameter/EvenToTheMoon.java
@@ -2,6 +2,10 @@ package method_parameter;
 
 public class EvenToTheMoon {
     public static void main(String[] args) {
-
+       System.out.println(getWeight(300));
+    }
+    public static double getWeight(int earthWeight) {
+        double weightMoon = 0.17 * earthWeight;
+        return weightMoon;
     }
 }
