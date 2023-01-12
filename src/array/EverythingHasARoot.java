720082b36ef03ec1b904185c042dce222b51204b
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-11 20:52:59 -0500

    Update

--- src/array/EverythingHasARoot.java
+++ src/array/EverythingHasARoot.java
@@ -2,6 +2,17 @@ package array;
 
 public class EverythingHasARoot {
     public static void main(String[] args) {
+        int[] array = {15, 64, 9, 51, 42};
+        printSqrt(array);
+    }
+
+    public static void printSqrt(int[] array) {
+        String elementSqrtString = "The square root of ";
+        for (int i = 0; i < array.length; i++) {
+            int element = array[i];
+            double elementSqrt = Math.sqrt(element);
+            System.out.println(elementSqrtString + element + " is " + elementSqrt);
+        }
 
     }
 }
