3e304af57c70ccbd5594b9a27fd7dfed3945b90a
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-13 20:59:24 -0500

    Remembering Geometry

---
+++ src/method_parameter/RemenberingGeometry.java
@@ -0,0 +1,12 @@
+package method_parameter;
+
+public class RemenberingGeometry {
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("The area of the circle is: " + pi * radius * radius);
+        System.out.println("The perimeter of the circle is: " + pi * diameter);
+    }
+}
