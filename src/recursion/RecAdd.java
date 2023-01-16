fb893da2fbc563534ab68718241a0efa4ea741d2
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-15 21:21:27 -0500

    Add all the numbers between 5 and 10

--- src/recursion/RecAdd.java
+++ src/recursion/RecAdd.java
@@ -2,6 +2,14 @@ package recursion;
 
 public class RecAdd {
     public static void main(String[] args) {
-
+        int result = sum(5, 10);
+        System.out.println(result);
+    }
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
     }
 }
