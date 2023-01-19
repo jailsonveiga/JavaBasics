0aa63151a77a21c7cd9b5672776df3ccaf51b5ab
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-19 15:59:02 -0500

    Unique Characters

--- src/for_loop/UniChar.java
+++ src/for_loop/UniChar.java
@@ -1,4 +1,20 @@
 package for_loop;
 
+import java.util.Scanner;
+
 public class UniChar {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        input = input.toLowerCase();
+        String temp = "";
+        for(int i =0;i<input.length();i++){
+            if(!temp.contains(Character.toString(input.charAt(i)))){
+                temp = temp+input.charAt(i);
+            }
+        }
+        System.out.println(temp);
+    }
 }
