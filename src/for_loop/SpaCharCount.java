58cfe60c8f2cdc1fcf8a42faeb602a0921a520c4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-20 16:01:10 -0500

    Space Character Count

--- src/for_loop/SpaCharCount.java
+++ src/for_loop/SpaCharCount.java
@@ -1,4 +1,22 @@
 package for_loop;
 
+import java.util.Scanner;
+
 public class SpaCharCount {
+    public static void main(String[] args){
+        int space_count=0,char_count=0;
+        Scanner s=new Scanner(System.in);
+        String input=s.nextLine();
+        s.close();
+        char[] result = input.toCharArray();
+        for(int i=0;i<result.length;i++){
+            if(Character.isWhitespace(result[i])){
+                space_count++;
+            }
+            else if(!Character.isDigit(result[i])){
+                char_count++;
+            }
+        }
+        System.out.println("No of spaces : " +space_count +" and characters : " +char_count);
+    }
 }
