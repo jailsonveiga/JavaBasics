39df128a3e12f51f0b928ae6a9060eb5a54c5785
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-23 16:06:22 -0500

    Reverse String

--- src/for_loop/RevStr1.java
+++ src/for_loop/RevStr1.java
@@ -1,4 +1,14 @@
 package for_loop;
 
+import java.util.Scanner;
+
 public class RevStr1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        input = input.toLowerCase();
+        StringBuffer inputSB = new StringBuffer(input);
+        System.out.print(inputSB.reverse());
+    }
 }
