c7b520652bd48f52910c29829525529309401692
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-24 16:08:32 -0500

    Reverse Number Version 1

--- src/while_loop/RevNum1.java
+++ src/while_loop/RevNum1.java
@@ -1,4 +1,20 @@
 package while_loop;
 
+import java.util.Scanner;
+
 public class RevNum1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int digit = sc.nextInt();
+        sc.close();
+        System.out.print("Reverse of the number is ");
+        if(digit == 0)
+            System.out.print(0);
+        else {
+            while(digit!=0){
+                System.out.print(digit%10);
+                digit /= 10;
+            }
+        }
+    }
 }
