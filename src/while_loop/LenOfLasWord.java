2d3da9271d58aee87e9ce0634a11320f77c0a37c
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-26 16:17:00 -0500

    The Length Of Last Word

--- src/while_loop/LenOfLasWord.java
+++ src/while_loop/LenOfLasWord.java
@@ -1,4 +1,31 @@
 package while_loop;
 
 public class LenOfLasWord {
+    public int lengthOfLastWord(final String a)
+    {
+        int len = 0;
+
+        /* String a is 'final'-- can not be modified
+           So, create a copy and trim the spaces from
+           both sides */
+        String x = a.trim();
+
+        for (int i = 0; i < x.length(); i++) {
+            if (x.charAt(i) == ' ')
+                len = 0;
+            else
+                len++;
+        }
+
+        return len;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        String input = "hello java  ";
+        LenOfLasWord gfg = new LenOfLasWord();
+        System.out.println("The length of last word is "
+                + gfg.lengthOfLastWord(input));
+    }
 }
